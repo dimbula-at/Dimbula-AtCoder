a78fcb66a17f6aab1e75add03ddb4baa430e3beb
yumi <yumi@DuckhousenoiMac.local>
2020-09-05 22:44:44 +0900

    https://atcoder.jp/contests/abl/submissions/17039903

---
+++ AtCoder/abl/abl_a.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int k;
+  int i;
+  cin >> k;
+
+for(int i=0; i<k; i++){
+  if(i<k){
+    cout << "ACL";
+  } else { 
+    cout << "\n";
+    }
+}
+  return 0;
+}
