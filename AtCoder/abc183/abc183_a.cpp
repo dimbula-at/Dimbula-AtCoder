8278b31c58cdd2b27da4ffad9e6d421f081de20f
yumi <yumi@DuckhousenoiMac.local>
2020-09-05 22:44:45 +0900

    https://atcoder.jp/contests/abc183/submissions/18130046

---
+++ AtCoder/abc183/abc183_a.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int x;
+  cin >> x;
+
+  if (x<0){
+      cout << "0" << "\n";
+  } else{
+      cout << x << "\n";
+  }
+
+  return 0;
+}
