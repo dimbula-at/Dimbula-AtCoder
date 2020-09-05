66f78b736ed950ad55e5bd90e55dc1d4d78ce621
yumi <yumi@DuckhousenoiMac.local>
2020-09-05 22:44:45 +0900

    https://atcoder.jp/contests/abl/submissions/17048716

--- AtCoder/abl/abl_b.cpp
+++ AtCoder/abl/abl_b.cpp
@@ -1,19 +1,18 @@
-#include <iostream>
+#include<iostream>
 #include<limits>
+#include<algorithm>
+#include<vector>
 using namespace std;
-
 int main() {
 
-  int64_t a,b,c,d;
-  cin >> a>>b>>c>>d;
+  int64_t a,b,c,d,upper,lower;
+  cin>>a>>b>>c>>d;
 
-  if(b<c or d<a){
-    cout << "No" << "\n";
-  } else if(b>=c or d>=a){
-    cout << "Yes" << "\n";
+  if(max(a,c) > min(b,d)){
+    cout<<"No\n";
+  }else{
+    cout<<"Yes\n";
   }
-  cerr<<INT_MAX<<"\n";
-  cerr<<INT64_MAX<<"\n";
-  return 0;
+
 }
 
